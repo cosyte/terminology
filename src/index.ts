606c06d550a7a3c5a4c7841c69a778cd1f4c8c18
Noah Schatz <noah.lane.schatz@gmail.com>
2026-07-30 15:52:46 -0400

    Version Packages (#23)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@
  * typeof VERSION; // => "string"
  * ```
  */
-export const VERSION: string = "0.0.3";
+export const VERSION: string = "0.0.4";
 
 // ── Value types ──────────────────────────────────────────────────────────────────────────────
 export { coding, codeableConcept, type Coding, type CodeableConcept } from "./common/coding.js";
