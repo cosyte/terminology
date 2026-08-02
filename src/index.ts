62844099f42761a392213b0d466fc0dc2f8148d8
Noah Schatz <noah.lane.schatz@gmail.com>
2026-08-02 15:19:22 +0000

    Revert "Version Packages (#32)"
    This reverts commit d318225e55284efd74007c3bae2151027882680c.

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@
  * typeof VERSION; // => "string"
  * ```
  */
-export const VERSION: string = "0.0.7";
+export const VERSION: string = "0.0.6";
 
 // ── Value types ──────────────────────────────────────────────────────────────────────────────
 export { coding, codeableConcept, type Coding, type CodeableConcept } from "./common/coding.js";
